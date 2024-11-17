220f7c6ac6bc9fbf17fd9ea50a1fe16400e1045c
Kartik Panjla <kr.panjla.3@gmail.com>
2024-11-17 18:43:38 +0530

    created some utilities

---
+++ src/app.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
+import e from 'cors';
+
+//Setting up server
+const app = express();
+
+//Setting some middlewares
+
+var corsOptions = {
+    origin: process.env.CORS_ORIGIN,
+    credendials: true
+  }
+app.use(cors(corsOptions))
+app.use(cookieParser())
+app.use(express.json({limit:"16kb"}))
+app.use(express.static("public"))
+app.use(express.urlencoded({extended:true, limit:"16kb"}))
+
+
+export {app}
--- src/index.js
+++ src/index.js
@@ -1,20 +1,21 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
-import express from 'express';
+import { app } from "./app.js";
 
 //Loading environment variables
 dotenv.config({path:'./env'})
 
-//Setting up server
-const app = express();
-
-app.listen(process.env.PORT, ()=>{
-    console.log(`Server is Up and Running on PORT ${process.env.PORT}....`)
+connectDB()
+.then(()=>{
+    app.on("error", (err)=>{
+        console.log(`Error:  ${err}`); 
+        throw err;
+    })
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`Server is Up and Running on PORT ${process.env.PORT}....`)
+    })
 })
-
-
-
-connectDB();
-
-
-
+.catch(()=>{
+    console.log("MongoDB connection failed " + console.error)
+    throw error;
+})
